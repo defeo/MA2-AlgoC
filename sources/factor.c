0be9a0f0fdf5d5e0b877623528b414051437a5a5
Luca De Feo <dfl@deagol>
2013-12-19 17:44:47 +0100

    Implemented p+1

--- sources/factor.c
+++ sources/factor.c
@@ -68,6 +68,71 @@ int p_minus_one(mpz_t res, const mpz_t N, unsigned long B, gmp_randstate_t rand)
   return !mpz_cmp(N, res);
 }
 
+/* Méthode p+1 de Williams */
+int p_plus_one(mpz_t res, const mpz_t N, unsigned long B, gmp_randstate_t rand) {
+  mpz_t exp, P, tmp;
+  // Comme dans la méthode p-1, on initialise l'exposant e à B!
+  mpz_inits(exp, P, tmp, NULL);
+  mpz_fac_ui(exp, B);
+  /*
+    Dans la suite, on travaille avec les points affines de la conique
+    de Pell d'équation x² - Δy² = 4 (avec Δ inconnu). On représente
+    ces points uniquement par leur abscisse.
+  */
+  // On initialise un point P au hasard
+  mpz_urandomm(P, rand, N);
+  /*
+    On calcule le multiple scalaire [e]P, par les formules de Lucas.
+    On initialize deux variables :
+      res <- 0
+      tmp <- P
+  */
+  long i;
+  mpz_set_ui(res, 2);
+  mpz_set(tmp, P);
+  /*
+    On fait un double-and-add gauche-droite. On garantit l'invariant
+    de boucle:
+      tmp = res + P
+  */
+  for (i = mpz_sizeinbase(exp, 2) - 1; i >= 0; i--) {
+    if (mpz_tstbit(exp, i)) {
+      /*
+	Si le bit vaut 1
+	  res <- res + tmp
+	  tmp <- 2*tmp
+      */
+      // Formule d'addition : x_{2n-1} = x_{n-1} * x_n - x
+      mpz_mul(res, res, tmp);
+      mpz_sub(res, res, P);
+      // Formule de dédoublement : x_{2n} = x_n^2 - 2
+      mpz_mul(tmp, tmp, tmp);
+      mpz_sub_ui(tmp, tmp, 2);
+    } else {
+      /* 
+	 Si le bit vaut 0
+	   res <- 2*res
+	   tmp <- res + tmp
+      */
+      // Addition
+      mpz_mul(tmp, res, tmp);
+      mpz_sub(tmp, tmp, P);
+      // Dédoublement
+      mpz_mul(res, res, res);
+      mpz_sub_ui(res, res, 2);
+    }
+    mpz_mod(res, res, N);
+    mpz_mod(tmp, tmp, N);
+  }
+  // On calcule pgcd(x(P)-2, N)
+  mpz_sub_ui(res, res, 2);
+  mpz_gcd(res, res, N);
+  mpz_clears(exp, P, tmp, NULL);
+
+  // On renvoie 0 si le test a trouvé un facteur non trivial
+  return !mpz_cmp(N, res);
+}
+
 /* Fonction qui affiche l'aide */
 void usage(char* prog) {
   fprintf(stderr, "Usage: %s [-a (rho|minusone|plusone)] [-B smoothness bound] [-s seed] N\n", prog);
@@ -124,6 +189,7 @@ int main(int argc, char** argv) {
       break;
     case 'p':
     case 'P':
+      status = p_plus_one(x, N, B, rand);
       break;
     case 'm':
     case 'M':
